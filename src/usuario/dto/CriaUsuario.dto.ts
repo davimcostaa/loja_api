0a4cfa66be6afc5b566b9c55454786b43e812d85
Davi Marques <davim.costa@outlook.com>
2023-11-06 11:35:05 -0300

    Criação da API

---
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, IsEmail, MinLength, IsNotEmpty } from "class-validator";
+
+export class CriaUsuarioDTO {
+
+    @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+    nome: string;
+
+    @IsEmail(undefined, {message: 'O email informado é inválido'})
+    email: string;
+
+    @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
+    senha: string;
+
+}
