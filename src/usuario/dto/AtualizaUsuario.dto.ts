157ac1a6bc121f2ef948a5c83d2ddb475e53a6f1
Davi Marques <davim.costa@outlook.com>
2023-11-13 11:58:32 -0300

    Cruds finalizados.

--- src/usuario/dto/AtualizaUsuario.dto.ts
+++ src/usuario/dto/AtualizaUsuario.dto.ts
@@ -1,16 +1,19 @@
-import { IsString, IsEmail, MinLength, IsNotEmpty } from "class-validator";
+import { IsString, IsEmail, MinLength, IsNotEmpty, IsOptional } from "class-validator";
 import { EmailEhUnico } from "../validacao/email_unico.validator";
 
-export class CriaUsuarioDTO {
+export class AtualizaUsuarioDTO {
 
     @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+    @IsOptional()
     nome: string;
 
     @EmailEhUnico({message: 'Já existe um usuário com o e-mail informado'})
     @IsEmail(undefined, {message: 'O email informado é inválido'})
+    @IsOptional()
     email: string;
 
     @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
+    @IsOptional()
     senha: string;
 
 }
